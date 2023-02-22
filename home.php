0dfb86d12c807b2b9f9ba179cb1bb100eb869d28
Devin Clemons <devincle@buffalo.edu>
2023-02-22 15:06:27 -0500

    Adding to main to have a base project

---
+++ home.php
@@ -0,0 +1,21 @@
+
+
+
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<div class="header">
+		<h1><span>The Fellers Calendar</span></h1>
+	</div>
+	<title>Login</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>	
+	LOGGED IN
+</body>
+</html>
+
+
