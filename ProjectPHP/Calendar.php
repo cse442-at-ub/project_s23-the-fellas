66fe69fd2f7290ac66e61ab1f3af8a4a86171d53
Jennifer Tsang <65834534+jtsang3@users.noreply.github.com>
2023-03-12 23:24:16 -0400

    Added modal Add Event form to Calendar.php, the actual calendar page #18

--- ProjectPHP/Calendar.php
+++ ProjectPHP/Calendar.php
@@ -15,6 +15,22 @@ $calendar->add_event('Holiday', '2023-03-16', 7);
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 	</head>
 	<body>
+    <!-- Pop-up modal form for adding events. -->
+    <div id="myModal" class="modal">
+
+        <!-- Modal content -->
+        <div class="modal-content">
+            <span class="close">&times;</span>
+            <p>Add event details here:</p>
+            <form action="addEvent.php" method="post">
+                <label for="event-title-input">Event Title:</label>
+                <input type="text" id="event-title-input" class="modal-input"><br>
+                <label for="event-datetime-input">Event Date and Time:</label>
+                <input type="datetime-local" id="event-datetime-input" class="modal-input"><br>
+                <input type="submit">
+            </form>
+        </div>
+    </div>
 	    <!-- <nav class="navtop"> -->
 	    <!-- <div> -->
 		<h1 id="mainHead">
@@ -25,7 +41,7 @@ $calendar->add_event('Holiday', '2023-03-16', 7);
 		<!-- </div> -->
 	    <!-- </nav> -->
 		<div class="sidenav">
-			<a id="sideAnchor">  <button id="sideButton" type="button" onclick="alert('Add Event')">Add Event</button></a>
+			<a id="sideAnchor">  <button id="sideButton" type="button">Add Event</button></a>
   			<h1 id="sideHeader">Sunday's Events</h1>
   			<ol>
     		<li>1:00 AM Testing</li>
@@ -40,5 +56,6 @@ $calendar->add_event('Holiday', '2023-03-16', 7);
 		  <div class="content home">
 			<?=$calendar?>
 		</div>
+              <script src="modal.js"></script>
 	</body>
 </html>
