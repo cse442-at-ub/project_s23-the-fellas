38bfc5b3a95b334b6a34e4b4517d999c58605cbd
Metro_Shroomin <dondreta@buffalo.edu>
2023-04-15 12:32:47 -0400

    Mobile Screen Adjustments #38
    Added rescaling for webpages when viewed on smaller screen sizes

--- ProjectPHP/modal.js
+++ ProjectPHP/modal.js
@@ -3,6 +3,7 @@ var modal = document.getElementById("myModal");
 
 // Get the button that opens the modal
 var btn = document.getElementById("sideButton");
+var btn2 = document.getElementById("topButton");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
@@ -11,7 +12,9 @@ var span = document.getElementsByClassName("close")[0];
 btn.onclick = function() {
   modal.style.display = "block";
 }
-
+btn2.onclick = function() {
+  modal.style.display = "block";
+}
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
