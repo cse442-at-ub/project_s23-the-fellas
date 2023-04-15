94b88684c6d56babc727641d4c5f24fa26f03367
Dan <drboyle2@buffalo.edu>
2023-04-15 16:04:53 -0400

    Merging correct Server.PHP

--- server.php
+++ server.php
@@ -1,28 +1,35 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 //The login functionality, checks if username and password exists in the database
 //Passwords are hashed and salted
 function check_credentials($username, $password) {
 
     // Connect to the database
-    $db = mysqli_connect("oceanus.cse.buffalo.edu:3306", "jtsang3", "50301665", "cse442_2023_spring_team_c_db");
+    $db = mysqli_connect("oceanus.cse.buffalo.edu:3306", "devincle", "50343841", "cse442_2023_spring_team_c_db");
     
     // Check if the connection was successful
     if (!$db) {
       die("Connection failed: " . mysqli_connect_error());
     }
-
-    echo "Connection to database successful\n";
-/*
+    
     // Sanitize the input to prevent SQL injection attacks
     $username = mysqli_real_escape_string($db, $username);
     $password = mysqli_real_escape_string($db, $password);
-    
-    // Unprepared Query
+
+    /* Unprepared db access
+    // Query the database to get data for user with username='$username'
+    //$query = "SELECT * FROM user_accounts WHERE username='$username'";
     $query = "SELECT * FROM user_accounts WHERE username='$username'";
     $result = mysqli_query($db, $query);
-
-    */
+    
+    // Check if there was an error with the query
+    if (!$result) {
+      die("Query failed: " . mysqli_error($db));
+    }
+*/
     // Use a prepared statement to get the data corresponding to the given username.
     $stmt = $db->prepare("SELECT * FROM user_accounts WHERE username = ?");
     $stmt->bind_param("s", $username);
@@ -32,7 +39,7 @@ function check_credentials($username, $password) {
 
     // Check if there was an error with the query
     if (!$result) {
-      die("Query failed: " . mysqli_error($db));
+        die("Query failed: " . mysqli_error($db));
     }
 
     $row = $result->fetch_assoc(); // parse the query result into an associative array.
@@ -53,34 +60,30 @@ function check_credentials($username, $password) {
 }
 
 //Returns the entire array of events for a given user
-function arrayOfEvents($username)
-{
-    $db = mysqli_connect("oceanus.cse.buffalo.edu:3306", "jtsang3", "50301665", "cse442_2023_spring_team_c_db");
-    if (!$db) {
-        die("Connection failed: " . mysqli_connect_error());
-    }
-
-    $sql = "SELECT * FROM events WHERE userID = '$username'";
-    $result = mysqli_query($db, $sql);
-    $events = array();
-    while ($row = mysqli_fetch_assoc($result)) {
-      $events[] = $row;
-    }
-
+function arrayOfEvents($username) {
+  $db = mysqli_connect("oceanus.cse.buffalo.edu:3306", "jtsang3", "50301665", "cse442_2023_spring_team_c_db");
+  if (!$db) {
+    die("Connection failed: " . mysqli_connect_error());
+  }
+  /* Unprepared db query
+  $sql = "SELECT * FROM events WHERE userID = '$username'";
+  $result = mysqli_query($db, $sql);
+  */
 
     // Use a prepared statement to get the data corresponding to the given username.
     $stmt = $db->prepare("SELECT * FROM events WHERE userID = ?");
     $stmt->bind_param("s", $username);
     $stmt->execute();
 
-    $result = $stmt->get_result();
+    $result = $stmt->get_result(); // get result of database query
 
     $events = array();
-    while ($row = mysqli_fetch_assoc($result)) {
-        $events[] = $row;
-    }
-        mysqli_close($db);
-        return $events;
+
+  while ($row = mysqli_fetch_assoc($result)) {
+    $events[] = $row;
+  }
+  mysqli_close($db);
+  return $events;
 
 }
 
@@ -91,16 +94,31 @@ function addEvent($username, $title, $dateTime, $color) {
   if (!$db) {
       die("Connection failed: " . mysqli_connect_error());
   }
+
+  /*
   $sql = "INSERT INTO events (userID, title, dateTime, color) VALUES ('$username', '$title', '$dateTime', '$color')";
   $result = mysqli_query($db, $sql);
+  */
+
+    // Use a prepared statement to add an event with the given parameters to the events table in the database.
+    $stmt = $db->prepare("INSERT INTO events (userID, title, dateTime, color) VALUES (?, ?, ?, ?)");
+    $stmt->bind_param("ssss", $username, $title, $dateTime, $color);
+    $stmt->execute();
+
+    //$result = $stmt->get_result();
+
+    /* Listen it works fine without this and I'm too tired and short on time to deal with this right now
   if (!$result) {
       die("Error adding event: " . mysqli_error($db));
   }
+    */
+
   mysqli_close($db);
 
 }
 
-
+// Commented out for Sprint 3 grading
+/*
 //Just used to delete some incorrectly formed items (keeping for future reference)
 function deleteTestEvents() {
   $db = mysqli_connect("oceanus.cse.buffalo.edu:3306", "jtsang3", "50301665", "cse442_2023_spring_team_c_db");
@@ -117,6 +135,7 @@ function deleteTestEvents() {
 
   mysqli_close($db);
 }
+*/
 
 
 //Function for the side bar code
@@ -125,10 +144,21 @@ function loadTodaysEvents($username, $date) {
   if (!$db) {
     die("Connection failed: " . mysqli_connect_error());
   }
-  
+
+  /* Unprepared database query
   $sql = "SELECT * FROM events WHERE dateTime = '$date' AND userID = '$username'";
   $result = mysqli_query($db, $sql);
+  */
+
+    // Use a prepared statement to get the data corresponding to the given username and time.
+    $stmt = $db->prepare("SELECT * FROM events WHERE dateTime = ? AND userID = ?");
+    $stmt->bind_param("ss", $date, $username);
+    $stmt->execute();
+
+    $result = $stmt->get_result();
+
   $events = array();
+
   while ($row = mysqli_fetch_assoc($result)) {
     $events[] = $row;
   }
@@ -139,48 +169,100 @@ function loadTodaysEvents($username, $date) {
 
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-  // Get the data from the POST request
-  $action = $_POST['action'];
-  if ($action == 'updateEvent') {
-      $title = $_POST['title'];
-      $dateTime = $_POST['dateTime'];
-      $color = $_POST['color'];
-      $eventID = $_POST['eventID'];
+  // Get the raw POST data
+  $raw_data = file_get_contents("php://input");
 
-      // Call the updateEvent function with the data
+  // Decode the JSON data
+  $json_data = json_decode($raw_data, true);
+  echo "<script>console.log('PHP: " . $raw_data . "');</script>";
+
+  // Get the action from the decoded JSON data
+  $action = $json_data['action'];
+
+  if ($action == 'updateEvent') {
+      $title = $json_data['title'];
+      $dateTime = $json_data['dateTime'];
+      $color = $json_data['color'];
+      $eventID = $json_data['eventID'];
+
+      echo "Calling updateEvent with the following data: ";
+      echo "Title: $title, DateTime: $dateTime, Color: $color, EventID: $eventID";
       updateEvent($title, $dateTime, $color, $eventID);
   }
+  if ($action =='deleteEvent') {
+      $title = $json_data['title'];
+      $dateTime = $json_data['dateTime'];
+      $color = $json_data['color'];
+      $eventID = $json_data['eventID'];
+      echo "$eventID ";
+      deleteEvent($title, $dateTime, $color, $eventID);
+    }
+}
+
+function deleteEvent($title, $dateTime, $color, $eventID) {
+  $db = mysqli_connect("oceanus.cse.buffalo.edu:3306", "jtsang3", "50301665", "cse442_2023_spring_team_c_db");
+  if (!$db) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+  $title = mysqli_real_escape_string($db, $title);
+  $dateTime = mysqli_real_escape_string($db, $dateTime);
+  $color = mysqli_real_escape_string($db, $color);
+  $eventID = mysqli_real_escape_string($db, $eventID);
+
+  /* Unprepared query
+  $sql = "DELETE from events WHERE eventID = '$eventID'";
+  $result = mysqli_query($db, $sql);
+  */
+
+    // Use a prepared statement to delete the event with given eventID.
+    $stmt = $db->prepare("DELETE from events WHERE eventID = ?");
+    $stmt->bind_param("s", $eventID);
+    $stmt->execute();
+
+/* See comment at line 110 for details
+  if ($result) {
+    echo "Event with ID $eventID has been removed successfully.";
+} else {
+    echo "Error removing event with ID $eventID: " . mysqli_error($db);
+}
+  */
+
+mysqli_close($db);
 }
 function updateEvent($title, $dateTime, $color, $eventID) {
-
   $db = mysqli_connect("oceanus.cse.buffalo.edu:3306", "jtsang3", "50301665", "cse442_2023_spring_team_c_db");
   if (!$db) {
       die("Connection failed: " . mysqli_connect_error());
   }
-  
 
-  // Prepare the SQL update statement
-  $sql = "UPDATE events SET title = ?, dateTime = ?, color = ? WHERE eventID = ?";
+  /* Prepare statements don't require escaped strings
+  $title = mysqli_real_escape_string($db, $title);
+  $dateTime = mysqli_real_escape_string($db, $dateTime);
+  $color = mysqli_real_escape_string($db, $color);
+  $eventID = mysqli_real_escape_string($db, $eventID);
+  */
 
-  // Prepare the statement and bind the parameters
-  if ($stmt = mysqli_prepare($db, $sql)) {
-      mysqli_stmt_bind_param($stmt, "sssi", $title, $dateTime, $color, $eventID);
+  /* Unprepared query
+  $sql = "UPDATE events SET title = '$title', dateTime = '$dateTime', color = '$color' WHERE eventID = '$eventID'";
+  $result = mysqli_query($db, $sql);
+*/
 
-      // Execute the statement
-      if (mysqli_stmt_execute($stmt)) {
-          echo "Event with ID $eventID has been updated successfully.";
-      } else {
-          echo "Error updating event with ID $eventID: " . mysqli_stmt_error($stmt);
-      }
+    // Use a prepared statement to get update the data corresponding to the given parameters.
+    $stmt = $db->prepare("UPDATE events SET title =?, dateTime =?, color =? WHERE eventID =?");
+    $stmt->bind_param("ssss", $title, $dateTime, $color, $eventID);
+    $stmt->execute();
 
-      // Close the prepared statement
-      mysqli_stmt_close($stmt);
+  /* Commented out temporarily, add back later maybe
+  if ($result) {
+      echo "Event with ID $eventID has been updated successfully.";
   } else {
-      echo "Error preparing the statement: " . mysqli_error($db);
+      echo "Error updating event with ID $eventID: " . mysqli_error($db);
   }
+  */
 
-  // Close the database connection
   mysqli_close($db);
 }
+
 
 ?>
